ab9bdada06056689e59db982ac607623376124d2
Yash Mehrotra <yashmehrotra95@gmail.com>
2015-08-27 13:42:49 +0530

    indexing is working

--- elasticer.cpp
+++ elasticer.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 #include <curl/curl.h>
 #include <string>
+#include <typeinfo>
 using namespace std;
 int main()
 {
     CURL *curl;
     CURLcode res;
-
-    string post_data = "{ \"index\" : { \"_index\" : \"tindex\", \"_type\" : \"type1\", \"_id\" : \"2\" } }\n{\"name\" : \"yash\" }\n";
+    const char* x="yash";
+    const char* post_data = "{ \"index\" : { \"_index\" : \"tindex\", \"_type\" : \"type1\", \"_id\" : \"3\" } }\n{\"name\" : \"batman\" }\n";
     cout <<post_data;
+    const char* es_host = "http://localhost:9200/_bulk";
     curl_global_init(CURL_GLOBAL_ALL);
 
     /* get a curl handle */ 
@@ -17,11 +19,11 @@ int main()
                   /* First set the URL that is about to receive our POST. This URL can
                    *        just as well be a https:// URL if that is what should receive the
                    *               data. */ 
-        curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:9200/_bulk");
-        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, post_data.c_str());
+        curl_easy_setopt(curl, CURLOPT_URL, es_host);
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, post_data);
 
         res = curl_easy_perform(curl);
-
+        cout<<"Type -> "<<typeid(x).name()<<endl;
         if (res != CURLE_OK) {
             fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
             curl_easy_cleanup(curl);
--- seeker.cpp
+++ seeker.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <string.h>
 
 #include <curl/curl.h>
 #include <libgearman/gearman.h>
@@ -22,9 +23,25 @@ const char* elasticsearch_bulk_url = "http://localhost:9200/_bulk";
 void* gearman_index(gearman_job_st *job, void *context, size_t *result_size, gearman_return_t *ret_ptr)
 {
     char* msg;
+    CURL *curl;
+    CURLcode curl_response;
+    curl_global_init(CURL_GLOBAL_ALL);
+    const char* es_string;
+    curl = curl_easy_init();
     const void* jobptr = gearman_job_workload(job);//this takes the data from the client
-    if (jobptr) {
+    if (jobptr && curl) {
         cout << "job: " << (char*) jobptr << std::endl;
+        es_string = (char*)jobptr;
+
+        curl_easy_setopt(curl, CURLOPT_URL, elasticsearch_bulk_url);
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, es_string);
+
+        curl_response = curl_easy_perform(curl);
+        if (curl_response != CURLE_OK) {
+            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(curl_response));
+            curl_easy_cleanup(curl);
+        }
+        curl_global_cleanup();
     }
     *ret_ptr = GEARMAN_SUCCESS ;
     *result_size = 6;
@@ -43,7 +60,7 @@ int main()
 
     gearman_return = gearman_worker_add_server(gearman_worker,
                                                gearman_host,
-                                               gearman_port)
+                                               gearman_port);
 
     gearman_return = gearman_worker_add_function(gearman_worker,
                                                  gearman_index_name,
@@ -51,6 +68,7 @@ int main()
                                                  gearman_index,
                                                  job_context);
     
+    cout<<"Server Started";
     // Try while true
     while(1) {
         gearman_return = gearman_worker_work(gearman_worker);
